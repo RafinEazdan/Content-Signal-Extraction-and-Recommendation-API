44b2e199f33f17c47dca29b8e1f990610a6fe034
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-02-25 01:16:28 +0600

    users table is created in the db

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel
-
+from pydantic import EmailStr
+from datetime import datetime
 
 class UserCreate(BaseModel):
     username: str
     email: str
     password: str
+
+
+class UserResponse(BaseModel):
+    email: EmailStr
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
