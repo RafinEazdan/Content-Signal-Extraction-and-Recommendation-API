a036fcb8d627099734173f9498fd736556f0e83e
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-02-27 16:06:46 +0600

    get channel details

--- app/services/get_channel.py
+++ app/services/get_channel.py
@@ -1,5 +1,79 @@
+from fastapi import HTTPException
+import requests
+
 from app.core.config import settings
+
 
 YT_API_KEY = settings.YT_API_KEY
 
 def get_channel(channel_handle):
+    search_url = "https://www.googleapis.com/youtube/v3/search"
+    search_params = {
+    "part": "snippet",
+    "q": channel_handle,
+    "type": "channel",
+    "maxResults": 1,
+    "key": YT_API_KEY
+}
+
+    search_res = requests.get(search_url, params=search_params).json()
+
+    if not search_res.get("items"):
+        raise HTTPException
+
+    CHANNEL_ID = search_res["items"][0]["id"]["channelId"]
+
+# Step 2: Get channel details
+    channel_url = "https://www.googleapis.com/youtube/v3/channels"
+    channel_params = {
+    "part": "snippet,statistics,contentDetails",
+    "id": CHANNEL_ID,
+    "key": YT_API_KEY
+}
+
+    res = requests.get(channel_url, params=channel_params).json()
+
+    if not res.get("items"):
+        raise HTTPException
+    
+
+    item = res["items"][0]
+
+    channel_name = item["snippet"]["title"]
+    subscriber_count = item["statistics"].get("subscriberCount", "Hidden")
+    uploads_playlist = item["contentDetails"]["relatedPlaylists"]["uploads"]
+
+# Step 3: Get all videos
+    videos = []
+    next_page = None
+
+    while True:
+        playlist_url = "https://www.googleapis.com/youtube/v3/playlistItems"
+        playlist_params = {
+        "part": "snippet,contentDetails",
+        "playlistId": uploads_playlist,
+        "maxResults": 50,
+        "pageToken": next_page,
+        "key": YT_API_KEY
+    }
+
+        data = requests.get(playlist_url, params=playlist_params).json()
+
+        if "items" not in data:
+            print("Playlist error:", data)
+            break
+
+        videos.extend(data["items"])
+        next_page = data.get("nextPageToken")
+
+        if not next_page:
+            break
+
+    video_list = [
+        {
+        "video_id": v["contentDetails"]["videoId"],
+        "title": v["snippet"]["title"],
+        "published_at": v["snippet"]["publishedAt"]
+        }
+        for v in videos
+]
