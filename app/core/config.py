913ec5242de7686a0ffaf72992b6a3c0ba9682ae
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-02-27 16:00:13 +0600

    config

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    YT_API_KEY = str
+    DATABASE_URL = str
+    SECRET_KEY = str
+    POSTGRES_PASSWORD = str
+    ACCESS_TOKEN_EXPIRE_MINUTES = int
+    ALGORITHM = str
+
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
+
---
+++ app/services/get_channel.py
@@ -0,0 +1,5 @@
+from app.core.config import settings
+
+YT_API_KEY = settings.YT_API_KEY
+
+def get_channel(channel_handle):
