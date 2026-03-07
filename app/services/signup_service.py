32a0824776a3dee26b292bb16c0d0bf81ea482bd
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-03-08 02:05:57 +0600

    check existing user

--- app/services/signup_service.py
+++ app/services/signup_service.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from psycopg import Connection
 from core.security import hash
 from services.otp_service import OTPService
@@ -9,10 +10,25 @@ class SignupService:
         self.db = db
         self.redis = redis
 
+    def check_existing_user(self, email):
+        try:
+            with self.db.cursor() as cursor:
+                cursor.execute("SELECT id FROM users WHERE email = %s;", (email,))
+            existing_user = cursor.fetchone()
+
+            if existing_user:
+                raise HTTPException(status_code=409, detail="Email already registered. Please login.")
+            
+        except Exception as e:
+             raise HTTPException(status_code=400, detail="Databse is not connected")
+
     async def send_otp(self, email, password):
         hashed_password = hash(password)
         otp_service = OTPService(self.redis)
         return await otp_service.generate_otp(email,hashed_password)
+    
+    # def signup_user(self, ):
+        
 
 
     
